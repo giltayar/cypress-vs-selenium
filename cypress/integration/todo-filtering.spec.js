acb7b5b7b1ed0c9bcf3e3f7442e33b85b9aee197
Gil Tayar <gil@tayar.org>
2019-02-04 15:22:17 +0200

    added support for fixtures using Redux

--- cypress/integration/todo-filtering.spec.js
+++ cypress/integration/todo-filtering.spec.js
@@ -1,6 +1,13 @@
 describe('filtering', function() {
   beforeEach(() => {
     cy.visit('http://todomvc-app-for-testing.surge.sh/')
+      // .then(window => {
+      //   window.__store.dispatch({type: 'ADD_TODO', text: 'Clean room'})
+      //   window.__store.dispatch({type: 'ADD_TODO', text: 'Learn JavaScript'})
+      //   window.__store.dispatch({type: 'ADD_TODO', text: 'Use Cypress'})
+
+      //   window.__store.dispatch({type: 'COMPLETE_TODO', id: 1})
+      // })
 
     cy.get('.new-todo').type('Clean room{enter}')
     cy.get('.new-todo').type('Learn JavaScript{enter}')
