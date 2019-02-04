533240e06c3bb82e5952c75f76d3aadc35e1b88c
Gil Tayar <gil@tayar.org>
2019-02-04 15:49:08 +0200

    visual test using eyes.cypress added

---
+++ cypress/integration/todo-visuals.spec.js
@@ -0,0 +1,54 @@
+/// <reference types="cypress" />
+
+describe('todo actions', () => {
+  before(() =>
+    cy.eyesOpen({
+      batchName: 'cypress vs selenium',
+      appName: 'cypress vs selenium',
+      testName: 'cypress vs selenium',
+      browser: [
+        {width: 1200, height: 1000},
+        // {width: 320, height: 1000},
+        // {width: 480, height: 1000},
+        // {width: 768, height: 1000},
+        // {width: 992, height: 1000},
+        // {width: 1200, height: 1000, name: 'firefox'},
+        // {width: 320, height: 1000, name: 'firefox'},
+        // {width: 480, height: 1000, name: 'firefox'},
+        // {width: 768, height: 1000, name: 'firefox'},
+        // {width: 992, height: 1000, name: 'firefox'},
+      ],
+    }),
+  )
+
+  after(() => cy.eyesClose())
+
+  beforeEach(() => {
+    cy.visit('http://todomvc-app-for-testing.surge.sh/')
+    // cy.visit('http://todomvc-app-for-testing.surge.sh/?different-title-color')
+
+    cy.get('.new-todo').type('Clean room{enter}')
+  })
+
+  it('should add a new todo to the list', () => {
+    // cy.get('label').should('have.text', 'Clean room')
+    // cy.get('.toggle').should('not.be.checked')
+    cy.eyesCheckWindow('new todo')
+  })
+
+  describe('toggling todos', () => {
+    it('should toggle test correctly', () => {
+      cy.get('.toggle').click()
+      // cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
+      cy.eyesCheckWindow('toggle todo')
+    })
+
+    it('should clear completed', () => {
+      cy.get('.toggle').click()
+
+      // cy.contains('Clear completed').click()
+      // cy.get('.todo-list').should('not.have.descendants', 'li')
+      cy.eyesCheckWindow('clear completed')
+    })
+  })
+})
